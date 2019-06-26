a070017882b1f5df9f3e91f19c73a9f8fdfc2c1b
Kandace Willcox <kiwdev1984@icloud.com>
2019-06-26 15:54:21 +1200

    returns all pokemon names

--- client/api/pokemon.js
+++ client/api/pokemon.js
@@ -3,7 +3,12 @@ import request from 'superagent'
 
 const apiBaseUrl = 'https://pokeapi.co/api/v2/pokemon/'
 
+// export function getPokemon(name) {
+//   return request.get(apiBaseUrl + name)
+//     .then(response => response.body)
+// }
+
 export function getPokemon(name) {
-  return request.get(apiBaseUrl + name)
-    .then(response => response.body)
+  return request.get(apiBaseUrl)
+    .then(response => response.body.results)
 }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { getPokemon } from '../api/pokemon'
 
-const pokemonName = 'bulbasaur'
+// const pokemonName = 'pokemon'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      pokemon: {}
+      pokemon: []
     }
   }
   componentDidMount() {
@@ -16,7 +16,7 @@ class App extends React.Component {
   }
 
   getPokemonData = () => {
-    getPokemon(pokemonName)
+    getPokemon()
       .then(pokemon => {
         this.setState({
           pokemon: pokemon
@@ -27,15 +27,64 @@ class App extends React.Component {
 
   render() {
     return (
-      <>
+       <>
         <h1>React development has begun!</h1>
-        {this.state.pokemon && this.state.pokemon.base_experience}
-
-
+        <ul>{this.state.pokemon.map(pokemon => {
+          return <li>{pokemon.name}</li>
+        })}
+        </ul>
       </>
+
     )
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+// GETS INDIVIDUAL POKEMON
+// const pokemonName = 'bulbasaur'
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       pokemon: {}
+//     }
+//   }
+//   componentDidMount() {
+//     this.getPokemonData()
+//   }
+
+//   getPokemonData = () => {
+//     getPokemon(pokemonName)
+//       .then(pokemon => {
+//         this.setState({
+//           pokemon: pokemon
+//         })
+//         console.log(pokemon)
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <h1>React development has begun!</h1>
+//         {this.state.pokemon && this.state.pokemon.base_experience}
+
+
+//       </>
+//     )
+//   }
+// }
 
 
 
