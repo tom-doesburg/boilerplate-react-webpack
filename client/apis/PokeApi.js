12ab1bcc738290605b5203e31242422d838cb157
danbez13 <daniel.bez13@gmail.com>
2019-06-26 15:43:13 +1200

    Pokeman

---
+++ client/apis/PokeApi.js
@@ -0,0 +1,8 @@
+import request from 'superagent'
+
+const apiBaseUrl = 'https://pokeapi.co/api/v2/'
+
+export function getPokemon(id) {
+    return request.get(apiBaseUrl + 'pokemon/' + id)
+        .then(response => response.body)
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import { getPokemon } from '../apis/PokeApi'
+
+const PokeId = 87
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pokemon: {
+        abilities: [{
+          ability :{
+          }
+        }]
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.getPokemonData()
+  }
+
+  getPokemonData = () => {
+    getPokemon(PokeId)
+      .then(pokemon =>{
+        console.log('data', pokemon);
+        
+        this.setState({
+          pokemon : pokemon
+        })
+      })
+  }
+
+
+
+  render(){
+    console.log('this is an apility')
+    return (
+      <>
+      <h1>Pokémon!</h1>
+      
+      <p>Pokemon: {this.state.pokemon.name}</p><br></br>
+      <p>Height: {this.state.pokemon.height}m</p><br></br>
+      Abilities:
+      <ul>
+      {this.state.pokemon.abilities.map(ability => {
+        return <li>{ability.ability.name}</li>
+      })}
+      </ul>
+      </>
+    )
+  }
 }
 
 export default App
